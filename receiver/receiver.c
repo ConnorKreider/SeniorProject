e2409ed0abab3be05a9e7008dca71194afbe4376
Connor Kreider <connorkreider26@gmail.com>
2019-04-25 10:41:32 -0400

    Moving over to work on a pi

--- receiver/receiver.c
+++ receiver/receiver.c
@@ -7,31 +7,116 @@
 *	the MCP4725 DAC, and that analog signal goes to the Push-Pull stage of the reciever
 */
 
-#include "receive.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>	// for testing avr
 #include <stdlib.h>
 
-void main()
+#define FOSC 1843200
+#define BAUD 9600
+//#define MYBURR FOSC/16/BAUD-1
+
+//void USART_init(unsigned int baud)
+//{
+//	//Set Baud rate
+//	UBRROH = (unsigned char)(ubrr>>8);
+//	UBRROL = (unsigned char)ubrr;
+
+//	// Enable Rx and Tx pins
+//	UCSROB = (1<<RXEN0)|(1<<TXEN0);
+
+//	// Frame format
+//	UCSROC = (1<<USBS0)|(3<<UCSZ00);
+
+//}
+
+//unsigned char USART_receive(void)
+//{
+	// Wait for an empty transmit buffer
+	//while(!(UCSRnA & (1<<UDREn)));
+
+	// Put data recieved into the buffer that sends data
+	//UDRn = data;
+
+	// Wait for data to be received
+//	while(!(UCSRnA & (1<<RXCn);
+
+	// Return data received
+//	return UDRn;
+//}
+
+int main(void)
 {
+	int blink = 0;
+	DDRD = 0x00;
+	PORTD = 0xFF;
+	DDRB = 0xFF;
+
+
+	//LED on
+	PORTB |= (1<<1);
+
+
+
 	// USART
-	USART_init();
+//	USART_init(BAUD);
+
+	// Start condition to enable communicate with DAC
+//	if ((TWSR & 0xF8) != START)
+
+//	while (!(TWCR & (1<<TWINT))); // Wait to see if start condition is transmitted
+	
+	// Check value of TWI status register
+	// If the status is different from Start go to Error	
+//	if ((TWSR & 0xF8) != START) {
+//		ERROR();
+//	}
+	
 
 	// Constantly read in value from BLUESMiRF and send to DAC
-	while(1) {
-		USART_receive();	
+//	while(1) {
+//		USART_receive();	
 	
+
 		// TO-DO // Need to change bits from 8 to 12 for DAC
+		// Pad the 2 most signifcant bits of the buffer
+		// This keeps the value from changing. 		
+
+		/*************************/
+		// Communicate to the DAC
 		
-		
-	
-		// Send to DAC
+/*		// Clear to start transmission of address
+		TWDR = SLA_W;		
+		TWCR = (1<<TWINT)|(1<<TWEN);
+
+		// Wait for flag to be set
+		while(!(TWCR & (1<<TWINT)));
+
+		// Check TWI status
+		if ((TWSR & 0xF8) != MT_SLA_ACK){
+			ERROR();
+		}
+
+		// Load the data into the register
+		TWDR = DATA;
+
+		// Clear the TWINT bit to start transmitting
+		TWCR = (1<<TWINT)|(1<<TWEN);
+
+		// Waits to make sure data was transmitted
+		while(!(TWCR & (1<<TWINT)));
+
+		// Check TWI status
+		if ((TWSR & 0xF8) != MT_DATA_ACK){
+			ERROR();
+		}
+
 	}
-	
+
+	// Have the data stop being transmitted
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
+*/	
 	return 0;
-
-
-
 
 
 }
--- transmitter/transmitter.c
+++ transmitter/transmitter.c
@@ -3,28 +3,82 @@
 *	
 *	This code is for the ATMega328P for the transmitter module. 
 *	The transmitter takes an Analog signal and uses the ADC to 
-*	convert it to a digital signal that can be sent to the reciever with the BLUESMiRF.
+*	convert it to a digital signal that can be sent to the reciever with the BLUESMiRF breakout board.
 */
 
-#include "transmitter.h"
+//#include "transmitter.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
+// For UART
+#define FOSC 1843200 // Clock speed (need to go back and change this based on data sheet)
+#define BAUD 9600	// Baud Rate
+//#define MYUBRR FOSC/16/BAUD-1
+
+void adc_init()
+{
+	// Set ARef equal to AVcc
+	ADMUX = (1 << REFS0)|(1<<REFS1);
+	// Enable ADC
+	ADCSRA = (1 << ADEN) | 7; // prescale = 128 (16MHz/128 = 125kHz) 
+}
+
+uint16_t adc_read(uint8_t input)
+{
+	ADMUC = (1 << REFS0) | (input & 0x0F); // Select the input and reference
+	ADCSRA |= (1 << ADSC);	// Start of conversion
+	
+	while (ADCSRA & (1 << ADSC));	// Wait for end of conversion
+
+	printf("ADC Value: %d\n",ADC);
+	return (ADC);	// Return converted value
+}
+
+void USART_init(unsigned int baud)
+{
+	// Set Baud Rate
+	UBRROH = (unsigned char) (baud >> 8);
+	UBRROL = (unsigned char) baud;
+
+	// Enable Rx and Tx pins
+	UCSROB = (1 << RXENOO) | (1 << TXEN0);
+
+	// Set up frame
+	// 8-bit data, 2 stop bits
+	UCSROC = (1 << USBS0) | (3 << UCSZOO);
+}
+
+void USART_transmit(uint16_t data)
+{
+	// Wait for empty transmit buffer
+	while( !(UCSRnA & (1 << UDREn));
+
+	// Put data into buffer and send the data
+	URDn = data;
+	// Send low byte
+	 // Send high byte need to make sure both are sent before starting next one
+}
+
 
 void main()
 {
-	uint8_t adc_val;
+	uint16_t adc_value;
 
 	// Initalize adc
 	adc_init();
 
 	// Initalize USART
-	USART_init();
+	USART_init(BAUD);
 
 	while(1) {
-		
-		adc_val = adc_read(PC0);	// Read value of adc from PC0, ADC pin		
+		// Sample rate is based on the frequency
+		// because the max frequency we filter til is 200Hz
+		// Sample rate is 10 * 200 = 2kSPS. Increase some more for overhead
+		// For bluetooth transmission so bits don't get dropped. 3kSPS
+
+
+		adc_value = adc_read(PC0);	// Read value of adc from PC0, ADC pin		
 		USART_transmit(adc_val);
 	}
 	
